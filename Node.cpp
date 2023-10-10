4fb9a9d70d06c98ced8c5007f3dbaf69ad964827
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-11 02:12:08 +0800

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,23 @@
+#include "Node.h"
+
+// Implement Node methods here
+
+// BigNumCalc.h
+#ifndef BIGNUMCALC_H
+#define BIGNUMCALC_H
+
+#include <list>
+#include <string>
+
+class BigNumCalc {
+public:
+    std::list<int> buildBigNum(std::string numString);
+    std::list<int> add(std::list<int> num1, std::list<int> num2);
+    std::list<int> sub(std::list<int> num1, std::list<int> num2);
+    std::list<int> mul(std::list<int> num1, std::list<int> num2);
+
+private:
+    // Add private members and methods as needed
+};
+
+#endif
