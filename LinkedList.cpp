e9ef8ef5d61323b2b5b1eb437ba8b1409944534d
ShaoTingYang <141378210+ShaoTingYang@users.noreply.github.com>
2023-10-11 02:10:11 +0800

    Create LinkedList.cpp

---
+++ LinkedList.cpp
@@ -0,0 +1,26 @@
+#include "LinkedList.h"
+#include "Node.h"
+
+// Implement LinkedList methods here
+
+// Node.h
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+public:
+    Node(int data);
+    ~Node();
+
+    int getData();
+    Node* getNext();
+    void setNext(Node* next);
+
+private:
+    int data;
+    Node* next;
+
+    // Add private members and methods as needed
+};
+
+#endif
